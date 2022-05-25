07f58cdcaebacd0f850d197f1eb366b0bc4adb8f
matthewoots <matthewoots@gmail.com>
2022-05-25 11:18:20 +0800

    Initialize

---
+++ src/main.cpp
@@ -0,0 +1,39 @@
+/*
+* main.cpp
+*
+* ---------------------------------------------------------------------
+* Copyright (C) 2022 Matthew (matthewoots at gmail.com)
+*
+*  This program is free software; you can redistribute it and/or
+*  modify it under the terms of the GNU General Public License
+*  as published by the Free Software Foundation; either version 2
+*  of the License, or (at your option) any later version.
+*
+*  This program is distributed in the hope that it will be useful,
+*  but WITHOUT ANY WARRANTY; without even the implied warranty of
+*  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+*  GNU General Public License for more details.
+* ---------------------------------------------------------------------
+*/
+
+#include <ros/ros.h>
+#include "trajectory_server_ros.h"
+
+#define KNRM  "\033[0m"
+#define KRED  "\033[31m"
+#define KGRN  "\033[32m"
+#define KYEL  "\033[33m"
+#define KBLU  "\033[34m"
+#define KMAG  "\033[35m"
+#define KCYN  "\033[36m"
+#define KWHT  "\033[37m"
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "bs_trajectory_node");
+    ros::NodeHandle nh("~");
+    int threads = 2;
+    ros::MultiThreadedSpinner spinner(threads);
+    spinner.spin();
+    return 0;
+}
